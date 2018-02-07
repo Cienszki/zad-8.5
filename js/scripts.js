80590b54e882fcaa39eca689ec5c3956b100cc71
Cienszki <wilq.wdz@gmail.com>
2018-02-07 18:54:44 +0100

    Add scripts

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = prompt('Enter base'),
+	 h = prompt('Enter height'),
+	 triangleArea = a*h/2;
+
+console.log('Triangle field with base '+ a +' and height ' + h +' equals ' + triangleArea);
+
+alert('Triangle field with base '+ a +' and height ' + h +' equals ' + triangleArea);
